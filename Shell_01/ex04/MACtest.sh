e013f393d3515b2479ff68508e0948d4525e238c
AlesiaTo <118931754+AlesiaTo@users.noreply.github.com>
2023-07-08 14:38:54 +0200

    Add files via upload

---
+++ Shell_01/ex04/MACtest.sh
@@ -0,0 +1,11 @@
+#ifconfig -a | grep ether | cut -d, -f 3
+#ifconfig -a | grep 'ether' | tr -s | cut -f 3
+#ifconfig -a | grep HWaddr
+#ifconfig eth0
+#ifconfig -a | grep ether 
+#--extended-regexp  
+#ip -brief link maddress
+#ifconfig eth9 | awk '/ether/ {print $2}' 
+#ifconfig -a | greb ether 
+# "Ethernet"
+~                  
---
+++ Shell_01/shell_00/ex07/shelleval/ex05/git_commit.sh
@@ -0,0 +1,3 @@
+!/bin/sh
+git log -3 --pretty=format:"%H" | tail -n 5
+
